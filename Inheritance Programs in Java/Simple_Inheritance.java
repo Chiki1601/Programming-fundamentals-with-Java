d9d2753edc6df9bc5b302e6f6f91797780e53f0f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-28 18:11:27 +0530

    Update Simple_Inheritance.java

--- Inheritance Programs in Java/Simple_Inheritance.java
+++ Inheritance Programs in Java/Simple_Inheritance.java
@@ -1,31 +1,28 @@
-// Online C++ compiler to run C++ program online
-#include <iostream>
-//#include<conio.h>
-int  main() {
-   int A[20],N,Temp,i,j;
- //  clrscr();
-   cout<<"Enter the number of elements:";
-   cin>>N;
-   cout<<"\n Enter the elements of th array";
-   for(i=0;i<N;i++)
-   {
-       cin>>A[i];
-   }
-       for(i=1;i<N;i++)
-       {
-           Temp = A[i];
-           j = i-1;
-           while(Temp<A[j]&&j>=0)
-           {
-               A[j+1] = A[j];
-               j = j-1;
-           }
-           A[j+1] = Temp[j]
-       }
-       cout<<"\n The sorted list is: \n";
-       for(i=0;i<N;i++){
-           cout<<A[i]<<"";
-       }
-   }
 
-    
+// Simple inheritance
+
+
+class Super_class
+{
+    int a = 10;
+    void Display_Super()
+    {
+        System.out.println("Display on Super Class: - Value of a is"+a);
+    }
+}
+
+class Sub_class extends Super_class{
+    void Display_sub()
+    {
+        System.out.println("\nDisplay on Sub Class:-  Value of a is"+a);
+    }
+}
+class Main    //change it while using in your device and save the main classname as   SingleInheritance
+{
+    public static void main(String args[])
+    {
+        Sub_class sub1 = new Sub_class();
+        sub1.Display_Super();
+        sub1.Display_sub();
+    }
+}
